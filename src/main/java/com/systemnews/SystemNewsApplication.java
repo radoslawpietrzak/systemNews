b44757046145b1f18d40beb79df286bdea552031
Radek <rpietrzak@wp.pl>
2016-08-28 21:41:07 +0200

    create new project

---
+++ src/main/java/com/systemnews/SystemNewsApplication.java
@@ -0,0 +1,12 @@
+package com.systemnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemNewsApplication.class, args);
+	}
+}
