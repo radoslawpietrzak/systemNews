1315194256dbefa00046eff4e723417ec645a5d6
Radek <rpietrzak@wp.pl>
2016-08-30 19:13:21 +0200

    Creating package model and classes News, Comment

---
+++ src/main/java/com/systemnews/model/Comment.java
@@ -0,0 +1,65 @@
+package com.systemnews.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by RADEK on 2016-08-30.
+ */
+@Document
+public class Comment {
+
+    @Id
+    public String id;
+    public String NewsId;
+    public String comment;
+    public String author;
+    public String data;
+
+    public Comment(String newsId, String comment, String author, String data) {
+        NewsId = newsId;
+        this.comment = comment;
+        this.author = author;
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNewsId() {
+        return NewsId;
+    }
+
+    public void setNewsId(String newsId) {
+        NewsId = newsId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/systemnews/model/News.java
@@ -0,0 +1,55 @@
+package com.systemnews.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by RADEK on 2016-08-30.
+ */
+@Document
+public class News {
+
+    @Id
+    public String id;
+    public String title;
+    public String text;
+    public String data;
+
+    public News(String title, String text, String data) {
+        this.title = title;
+        this.text = text;
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
